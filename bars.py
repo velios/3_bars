e6cec9111bda7c3ebc1f9119ec2e51e286378ad9
velios <prostozapomnit@gmail.com>
2017-08-08 21:37:50 +0300

    Add command line properties input and minor changes
    Rewrite input scheme
    Add -t flag to test without keyboard input

--- bars.py
+++ bars.py
@@ -1,5 +1,6 @@
 import json
 import os
+import argparse
 from math import hypot
 
 
@@ -11,7 +12,8 @@ def load_json_data(filepath, encoding='utf-8'):
 
 
 def pretty_print_json(prepared_json_data):
-    return json.dumps(prepared_json_data, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+    return json.dumps(prepared_json_data, sort_keys=True, indent=4,
+                      separators=(',', ': '), ensure_ascii=False)
 
 
 def get_biggest_bar(bars_data):
@@ -39,7 +41,8 @@ def get_closest_bar(bars_data, longitude, latitude):
         bar_latitude = float(bar['Latitude_WGS84'])
         longitude_difference = longitude - bar_longitude
         latitude_difference = latitude - bar_latitude
-        distance_between_me_and_bar = hypot(longitude_difference, latitude_difference)
+        distance_between_me_and_bar = hypot(longitude_difference,
+                                            latitude_difference)
         if closest_bar_distance is None:
             closest_bar_distance = distance_between_me_and_bar
             closest_bar = bar['Name']
@@ -47,27 +50,41 @@ def get_closest_bar(bars_data, longitude, latitude):
             if closest_bar_distance > distance_between_me_and_bar:
                 closest_bar_distance = distance_between_me_and_bar
                 closest_bar = bar['Name']
-
     return closest_bar, int(closest_bar_distance*100000)
 
 
 if __name__ == '__main__':
-    # Пропишете путь до списка московских баров в JSON формате с сайта https://data.mos.ru/opendata/7710881420-bary
-    PATH_TO_DATA_FILE = 'data-2897-2016-11-23.json'
+    parser_description = """
+    Скрипт на вход принимает путь до файла содержащих информацию о барах Москвы в JSON формате и определяет
+    самый большой, маленький и ближайший к точке назначения бар
+    Файл для примера можно скачать на https://data.mos.ru/
+    """
+    cmd_argument_parser = argparse.ArgumentParser(description=parser_description)
+    cmd_argument_parser.add_argument('filepath', help='Файл с данными о барах Москвы в JSON формате', type=str)
+    cmd_argument_parser.add_argument('-t', '--test', action='store_true', help='Не потребуется вводить данные с клавиатуры. Тестовый режим.')
+    cmd_arguments = cmd_argument_parser.parse_args()
 
-    try:
-        bars_data = load_json_data(PATH_TO_DATA_FILE, encoding='cp1251')
+    bars_data = load_json_data(cmd_arguments.filepath, encoding='cp1251')
 
-        current_longitude = float(input('Пожалуйста введите долготу(например 37.566316): '))
-        current_latitude = float(input('Пожалуйста введите широту(например 55.723065): '))
+    if not cmd_arguments.test:
+        current_longitude = float(input('Пожалуйста введите долготу(например 37.566316):'))
+        current_latitude = float(input('Пожалуйста введите широту(например 55.723065):'))
+    else:
+        current_longitude = 37.566316
+        current_latitude = 55.723065
+        print('Тестовый режим. Заранее заданные значения долготы:{} и широты:{}'.format(current_latitude,current_longitude))
 
-        biggest_bar, biggest_bar_number_of_seats = get_biggest_bar(bars_data)
-        smallest_bar, smallest_bar_number_of_seats = get_smallest_bar(bars_data)
-        closest_bar, closest_bar_distance = get_closest_bar(bars_data, current_longitude, current_latitude)
+    biggest_bar, biggest_bar_number_of_seats = get_biggest_bar(bars_data)
+    smallest_bar, smallest_bar_number_of_seats = get_smallest_bar(bars_data)
+    closest_bar, closest_bar_distance = get_closest_bar(bars_data,
+                                                        current_longitude, current_latitude)
 
-        print('Самый вместительный бар это {0} на {1} мест'.format(biggest_bar, biggest_bar_number_of_seats))
-        print('Самый маленький бар это {0} на {1} мест'.format(smallest_bar, smallest_bar_number_of_seats))
-        print('Ближайший к вам бар это {0} на расстоянии {1} метра'.format(closest_bar, closest_bar_distance))
-        print('Спасибо что воспользовались приложением.')
-    except:
-        print('Что-то пошло не так. Проверьте правильно ли вы указали путь до файла.')
+
+    print('\nСамый вместительный бар это {0} на {1} мест'.format(biggest_bar,
+                                                               biggest_bar_number_of_seats))
+    print('Самый маленький бар это {0} на {1} мест'.format(smallest_bar,
+                                                           smallest_bar_number_of_seats))
+    print('Ближайший к вам бар это {0} на расстоянии {1} метра\n'.format(closest_bar,
+                                                                       closest_bar_distance))
+    print('Спасибо что воспользовались приложением.')
+
